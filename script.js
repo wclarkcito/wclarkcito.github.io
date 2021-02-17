0bfe4d524fb0ca2f8df12e789cc0fa3238b6ef41
warren clark <wclarkcito@gmail.com>
2021-02-17 15:03:27 -0800

    added link to project 1 on landing page

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+
+    var projectLink = "https://eroux13.github.io/Cryptelligent"
+    $("#project1").append(projectLink);
+
+
+
+
+
+
+    console.log(projectLink)
+
+})
+
+
+
+
+
