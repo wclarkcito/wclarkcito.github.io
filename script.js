60e56ec9b188747341736c44ce1d64d18bf2df2b
warren clark <wclarkcito@gmail.com>
2021-04-12 17:22:55 -0700

    added project 2 to projects page

--- script.js
+++ script.js
@@ -1,17 +1,6 @@
-$(document).ready(function () {
-
-
-    var projectLink = "https://eroux13.github.io/Cryptelligent"
-    $("#project1").append(projectLink);
 
 
 
-
-
-
-    console.log(projectLink)
-
-})
 
 
 
